912879d62b0a20c026e73f8f58e79bfdb2fa81fa
Mischa Hermans <mischahermans3@gmail.com>
2025-07-10 19:07:37 +0200

    Refactor ARForecaster to use statsmodels AutoReg with automatic lag selection

--- models/ar_model.py
+++ models/ar_model.py
@@ -1,26 +1,36 @@
 import numpy as np
 import pandas as pd
+from statsmodels.tsa.ar_model import AutoReg, ar_select_order
 from sklearn.metrics import mean_absolute_error, root_mean_squared_error
 
 
 class ARForecaster:
-    def __init__(self):
+    def __init__(self, max_lag: int = 20, ic: str = 'aic'):
         """
-        AR forecaster (random walk model).
-        Always forecasts the last observed value forward.
+        AutoRegressive (AR) model using statsmodels.
+
+        Args:
+            max_lag (int): Maximum number of lags to consider.
+            ic (str): Information criterion for lag selection ('aic' or 'bic').
         """
-        self.last_value = None
+        self.max_lag = max_lag
+        self.ic = ic
+        self.model = None
+        self.fitted_model = None
 
     def fit(self, y_train: pd.Series):
-        """Memorize the last observed value."""
-        self.last_value = y_train.iloc[-1]
+        """Select optimal lag and fit AR model on training data."""
+        selected_order = ar_select_order(y_train, maxlag=self.max_lag, ic=self.ic).ar_lags
+        self.model = AutoReg(y_train, lags=selected_order, old_names=False)
+        self.fitted_model = self.model.fit()
 
     def predict(self, steps: int) -> np.ndarray:
-        """Return a flat forecast using the last value."""
-        return np.full(steps, self.last_value)
+        """Forecast the next `steps` values using fitted AR model."""
+        return self.fitted_model.predict(start=len(self.fitted_model.model.endog), 
+                                         end=len(self.fitted_model.model.endog) + steps - 1)
 
     def evaluate(self, y_train: pd.Series, y_test: pd.Series) -> dict:
-        """Fit and evaluate against test data."""
+        """Fit model and evaluate on test data."""
         self.fit(y_train)
         preds = self.predict(len(y_test))
         return {
